f4ca80222b21bda10c14ae33450f41211e0635f4
GabrielCFernandes <gabriel.caetano@b2list.com>
2023-03-24 23:46:12 -0300

    feat/loading(adicionando componente de carregamento

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Loading from "./components/loading";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Teste</div>,
+    element: <Loading/>,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
