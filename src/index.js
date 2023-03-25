1853a2e1835c06d48f16ce5015e30f373415bef2
Lucas Vinicius <Lucas.elias@gow solucoes.com>
2023-03-25 00:21:34 -0300

    feat: criação da base da tela home

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Loading from "./components/loading";
+import Loading from "./components/Loading";
+import Home from "./Pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Loading/>,
+    element: <Home/>,
   },
 ]);
 
