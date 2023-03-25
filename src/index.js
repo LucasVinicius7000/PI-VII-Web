a1ff2b04930fb55966bcc9bb82db6869eb731e2b
Lucas Vinicius <Lucas.elias@gow solucoes.com>
2023-03-25 11:58:52 -0300

    fix: removido importação não utilizada e correção da home

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Home from "./pages/Home";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Loading/>,
+    element: <Home/>,
   },
 ]);
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,3 @@
-import ModalAviso from "../../components/ModalAviso"
 
 export default function Home(){
     return <div>
