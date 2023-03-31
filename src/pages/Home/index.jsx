20a7f2e93abb336da22128724f3d87fd149b251f
Lucas Elias <lucasvinicius7000@gmail.com>
2023-03-31 01:25:07 +0000

    Esse commit é um teste de pipeline.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import CardCategoria from "../../components/CardCategoria";
 import CardProduto from "../../components/CardProduto";
 export default function Home() {
-  return <div>Alo alo alo alo alo</div>;
+  return <div>Testando 1..2..3</div>;
 }
