6f6c365b02129f1803b8d927cc3973d965065c85
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-04-21 13:36:59 -0300

    feature: criado hook retorna a localização usuário

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import CardCategoria from "../../components/CardCategoria";
 import CardEstabelecimento from "../../components/CardEstabelecimento";
 import CardProduto from "../../components/CardProduto";
+import useGeolocation from "../../hooks/useGeolocation";
+
 
 export default function Home() {
   return <div></div>;
