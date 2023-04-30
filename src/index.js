83f4ca154b48a48d193d8014c873c1ee8a399472
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-04-30 20:15:21 -0300

    criação de contexto para informações do usuário

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Loading from "./components/Loading";
 import Home from "./pages/Home";
-import SolicitaEndereco from "./components/SolicitaEndereco";
+import Endereco from "./components/Endereco";
+import UserContextProvider from "./contexts/userContext";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <SolicitaEndereco>
-      <Home />
-    </SolicitaEndereco>,
+    element: <Home />
+      
+  },
+  {
+    path: "/login",
+    element: <div>Tela de login deve vir aqui</div>
+  },
+  {
+    path: "/usuario/cadastro",
+    element: <div>Tela de cadastro do cliente deve vir aqui</div>
+  },
+  {
+    path: "/estabelecimento/cadastro",
+    element: <div>Tela de cadastro do estabelecimento deve vir aqui</div>
   },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RouterProvider router={router} />
+  <UserContextProvider>
+    <RouterProvider router={router} />
+  </UserContextProvider>
 );
