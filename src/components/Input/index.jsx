0d929c96c5ba8f95e4f721e5eea9afd713581407
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-04-30 23:06:58 -0300

    feat: tela de login do cliente em andamento

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,19 @@
+import styles from "./styles.module.css";
+
+export default function Input
+({
+startIcon, 
+placeholder, 
+endIcon, 
+onChange,
+hasError
+})
+{
+    
+    return <div className={styles.container} id={hasError && styles.containerError}>
+        {startIcon}
+        {startIcon && <div className={styles.verticalDeco}></div>}
+        <input onChange={onChange} placeholder={placeholder} id={hasError && styles.inputError}></input>
+        {endIcon}
+    </div>
+}
