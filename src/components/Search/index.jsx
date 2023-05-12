d1047f334ec5269a48ce8a3a9ccf5c1d47c3d845
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-05-12 09:38:36 -0300

    correção de conflito de merge

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,11 @@
 import styles from "./styles.module.css";
 import { useState } from "react";
 
-export default function Search({ decoIcon, searchIcon, onSearch, placeholder, value = "", listItemsResult = [], onChange, onClickItemList }) {
+export default function Search({ decoIcon, searchIcon, onSearch, placeholder, value = "", listItemsResult = [], onChange, onClickItemList, width, height, paddingLeft, alignItems, paddingBottom, text }) {
 
     
-    return <div className={styles.container}>
-        <div className={styles.containerInput} id={listItemsResult.length > 0 ? styles.borderAdjust : undefined}>
+    return <div className={styles.container} style={{ width: `${width}px`, height: `${height}px`, paddingLeft: `${paddingLeft}px`, alignItems: `${alignItems}px`, paddingBottom: `${paddingBottom}px`}}>
+        <div className={styles.containerInput} id={listItemsResult.length > 0 ? styles.borderAdjust : ""}>
             <span id={styles.icon}>{decoIcon}</span>
             <input value={value} placeholder={placeholder} className={styles.input} onChange={onChange}></input>
             <span onClick={onSearch}>{searchIcon}</span>
