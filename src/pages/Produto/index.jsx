7eb159f2315b13e7121e9e73deee1efb19d229c0
Lucas Vinicius <lucasvinicius7000@gmail.com>
2023-06-18 18:24:58 -0300

    page produto

--- src/pages/Produto/index.jsx
+++ src/pages/Produto/index.jsx
@@ -3,22 +3,27 @@ import { useNavigate, useParams } from "react-router-dom"
 import styles from "./styles.module.css";
 import { useEffect } from "react";
 import api from "../../services/Api";
-import { ToastError } from "../../utils/Toast";
+import { ToastError, ToastSucess } from "../../utils/Toast";
 import { useState } from "react";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/userContext";
 import InputNumber from "./../../components/InputNumber";
 import Input from "./../../components/Input";
 import Button from "./../../components/Button";
+import ModalAviso from "./../../components/ModalAviso";
 
 export default function Produto() {
 
+    const navigate = useNavigate();
     const { id } = useParams();
     const [produto, setProduto] = useState(null);
-    const { userToken } = useContext(UserContext);
+    const { userToken, clienteId } = useContext(UserContext);
     const [quantidadeUnitaria, setQuantidadeUnitaria] = useState(0);
     const [quantidadeEmKg, setQuantidadeEmKg] = useState(0);
     const [total, setTotal] = useState(0);
+    const [pedidoAtual, setPedidoAtual] = useState(undefined);
+    const [hasPedidoDif, setHasPedidoDif] = useState(false);
+    const [observacao, setObservacao] = useState('');
 
     useEffect(() => {
         debugger;
@@ -49,12 +54,109 @@ export default function Produto() {
         }
     }, [id, userToken, produto]);
 
-    const navigate = useNavigate();
+    useEffect(() => {
+        if (clienteId != null) {
+            const getPedidoAtual = async () => {
+                try {
+                    let response = await api.get(`pedido/atual?ClienteId=${clienteId}`);
+                    if (response?.data?.isSucessful) setPedidoAtual(response.data?.data);
+                    else setPedidoAtual(null);
+                } catch (error) {
+                    setPedidoAtual(null);
+                }
+            }
+            getPedidoAtual();
+        }
+    }, [clienteId]);
 
-    return <div className={styles.container}>
+
+    const verifyCanSubmit = async () => {
+        debugger;
+        if (pedidoAtual != null) {
+            if (produto?.estabelecimentoId !== pedidoAtual?.estabelecimentoId) {
+                setHasPedidoDif(true);
+            }
+            else await handleCreatePedidoOrAddProduto();
+        }
+        else {
+            await handleCreatePedidoOrAddProduto()
+        };
+    };
+
+    const handleCancelPedido = async () => {
+        try {
+            let response = await api.patch(`pedido/cancelar?pedidoId=${pedidoAtual?.id}`);
+            if (response.data.isSucessful) {
+                ToastSucess(`Pedido #${pedidoAtual?.atual} cancelado com sucesso.`);
+            }
+            else ToastError("Falha ao cancelar pedido atual.");
+        } catch (error) {
+            ToastError("Falha ao cancelar pedido atual.");
+        }
+    }
+
+    const handleCreatePedidoOrAddProduto = async () => {
+
+        if (produto == null) {
+            ToastError("Produto inválido, impossível adicionar.");
+            return;
+        }
+
+        if (produto?.vendidoPor == 0 && quantidadeUnitaria <= 0) {
+            ToastError("A quantidade unitária deve ser maior que 0.");
+            return;
+        }
+
+        if ((produto?.vendidoPor == 1 || produto?.vendidoPor == 2) && quantidadeEmKg <= 0) {
+            ToastError("A quantidade deve ser maior que 0.");
+            return;
+        }
+
+        let quantidade, valorUnitarioKgL;
+
+        if (produto?.vendidoPor == 0) quantidade = quantidadeUnitaria;
+        else if (produto?.vendidoPor == 1 || produto?.vendidoPor == 2) quantidade = quantidadeEmKg;
+
+        if (produto?.valorComDesconto == null) valorUnitarioKgL = produto?.valorUnitario;
+        else if (produto?.valorComDesconto == null) valorUnitarioKgL = produto?.valorComDesconto;
+
+        try {
+            debugger;
+            let response = await api.post('pedido/criarOUeditar', {
+                clienteId: clienteId,
+                observacao: observacao,
+                produtoIdOriginal: produto?.id,
+                quantidadeUnidadeKgLitro: quantidade,
+                valorUnitarioKgL: valorUnitarioKgL,
+            });
+            if (response.data.isSucessful) ToastSucess(`Produto ${produto?.nome} adicionado ao pedido com sucesso.`);
+            else ToastError("Falha ao adicionar pedido.");
+        } catch (error) {
+            ToastError("Falha ao adicionar pedido.");
+        }
+    }
+
+    if (pedidoAtual !== undefined) return <div className={styles.container}>
         <HeaderGeral
             onClickBack={() => navigate(-1)}
         />
+        <ModalAviso isOpen={hasPedidoDif}>
+            <div className={styles.modalError}>
+                <div>
+                    Você já possui itens de outro estabelecimento em seu pedido atual, deseja cancelar o pedido e iniciar outro?
+                </div>
+                <div className={styles.options}>
+                    <div onClick={async () => {
+                        await handleCancelPedido();
+                        await handleCreatePedidoOrAddProduto();
+                        setHasPedidoDif(false);
+                    }}>Sim, cancele o pedido atual e crie um novo.</div>
+                    <div onClick={() => {
+                        setHasPedidoDif(false);
+                    }}>Não, mantenha o pedido atual.</div>
+                </div>
+            </div>
+        </ModalAviso>
         <main className={styles.main}>
             <div className={styles.productItself}>
                 <div>
@@ -79,7 +181,7 @@ export default function Produto() {
 
             <div className={styles.aside}>
                 <h3>Observação</h3>
-                <Input type="text" placeholder={"Digite aqui"} backgroundColor={"#eee"} />
+                <Input type="text" onChange={(e) => setObservacao(e.target.value)} placeholder={"Digite aqui"} backgroundColor={"#eee"} />
                 {
                     produto?.vendidoPor == 0 && <InputNumber noFloats limit={produto?.quantidadeEstoque} setValue={(val) => {
                         if (val <= produto?.quantidadeEstoque) setQuantidadeUnitaria(parseInt(val))
@@ -97,7 +199,7 @@ export default function Produto() {
                     }} titleInput="Quantidade em Kg" />
                 }
 
-                {       
+                {
                     produto?.vendidoPor == 2 && <InputNumber interval={0.5} limit={produto?.quantidadeEstoque} setValue={(val) => {
                         if (val <= produto?.quantidadeEstoque) setQuantidadeEmKg(val)
                         else if (val >= produto?.quantidadeEstoque) {
@@ -111,7 +213,12 @@ export default function Produto() {
                         Valor Total: {'\u00A0'}<span>R${parseFloat(total).toFixed(2)}</span>
                     </h3>
                     <div>
-                        <Button width={'100%'} text='Adicionar produto'></Button>
+                        <Button
+                            width={'100%'}
+                            onClick={() => {
+                                verifyCanSubmit();
+                            }}
+                            text='Adicionar produto'></Button>
                     </div>
                 </div>
             </div>
